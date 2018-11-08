4ed6f21c39b5046737ea4682ee731b451f9482d9
Segei <44839185+Segei@users.noreply.github.com>
2018-11-08 12:06:03 +0500

    Add files via upload

---
+++ Lab3_1z/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab3_1z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] mass = new int[20];
+            int f=0;
+            for (int i = 0; i < 20; i++)
+            {
+               
+                mass[i]=rand.Next(-10, 10);
+                Console.WriteLine(mass[i]);
+            }
+            for(int i = 0; i < 20; i++)
+            {
+                if (mass[i] > 0)
+                {
+                    f++;
+                }
+            }
+            Console.WriteLine("Положительных чисел: " + f);
+            Console.ReadKey();
+        }
+    }
+}
